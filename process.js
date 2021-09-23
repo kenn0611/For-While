0b4bb7be8dd5850934826c6c57b3a47efc87c2da
kenn0611 <83709575+kenn0611@users.noreply.github.com>
2021-09-23 19:07:25 -0400

    Initial Change

--- process.js
+++ process.js
@@ -1,13 +1,13 @@
-let wrap = document.getElementsByClassName("wrapper")[0];
+var wrap = document.getElementsByClassName("wrapper")[0];
 
 let dragonHealth = 10;
 let userHealth = 5;
 
 //this wile loop runs wile both are alive
-let dragRandDamage;
 let randDamage;
+let dragRandDamage;
 while (dragonHealth > 0 && userHealth > 0) {
-    let damage = window.prompt("Enter the number of hits you are going to attempt to hit the dragon Max damage upto 5");
+    var damage = window.prompt("Enter the number of hits you are going to attempt to hit the dragon Max damage upto 5");
 
     //random damage generator
     randDamage = Math.floor(Math.random() * damage) + 1;
